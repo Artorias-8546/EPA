24dc88cd78ebe238fb264ed32b8c2b36b1636679
PedroMTG <pedroporfirio2006@gmail.com>
2023-09-30 15:20:27 -0300

    img

--- header.php
+++ header.php
@@ -1,6 +1,6 @@
 <header>
     <div class="container-header">
-    <a href="index.php"><img src="logo.img" alt=""></a>
+    <a href="index.php"><img src="img/logo.jpg" alt=""></a>
     <i class="ph ph-star"></i>
     </div>
   
