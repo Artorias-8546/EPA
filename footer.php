5c8498d4b956fcf3294b24012e614e1ba75e5ef2
Artorias-8546 <luisporfirio2006@gmail.com>
2023-09-30 14:15:24 -0300

    update1.4

--- footer.php
+++ footer.php
@@ -1,7 +1,8 @@
 <footer class= "container-footer">
     <div class="bloco-primario">
-        
+
     </div>
+    
     <div class="bloco-central"></div>
     <div class="borda-footer"></div>
 </footer>
