430acf95db7f1ca96f811025a474196e2f00617e
PedroMTG <pedroporfirio2006@gmail.com>
2023-09-30 16:06:11 -0300

    main

--- main.php
+++ main.php
@@ -1,3 +1,13 @@
 <main>
-    
+    <div class="container-main">
+
+        <div class="container-esquerda">
+            <div class="esquerda">
+            </div>
+            <button type="submit">Começar agora!</button>
+        </div>
+
+        <div class="direita">
+        </div>
+    </div>
 </main>
