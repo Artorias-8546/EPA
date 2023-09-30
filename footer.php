4e732c04246e3c3ccff85670caa4620c9f01a546
Artorias-8546 <luisporfirio2006@gmail.com>
2023-09-30 15:18:13 -0300

    gfdhgfds

--- footer.php
+++ footer.php
@@ -3,6 +3,11 @@
 
     </div>
     <div class="bloco-central">
+        <div class="text">
+             O “Nome do app” é um aplicativo Web 
+            para sua saúde, somos uma impresa de
+            tecnologia e inovação. 
+        </div>
         <div><img src="logo.jpg" alt=""></div>
     </div>
 </footer>
