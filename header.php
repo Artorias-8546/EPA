79fbb844c84315840f8bfbc0ea134f10ee3138ed
Artorias-8546 <luisporfirio2006@gmail.com>
2023-10-05 14:44:37 -0300

    g

--- header.php
+++ header.php
@@ -1,9 +1,7 @@
 <header>
-    <div class="container-header">
         <img src="img/logo.jpg" alt="">
 
         <a href="javascript:exibirModal()">
             <i class="ph ph-star"></i>
         </a>
-    </div>
 </header>
