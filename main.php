da7b7334b16d039bb7354570c054cf1c48ecbb87
LeticiaCoelhoF <leluke20@gmail.com>
2023-10-06 12:20:57 -0300

    Adicionando imagem aos botões

--- main.php
+++ main.php
@@ -24,50 +24,57 @@
             <!-- <div class="mensagem" for="t-button"></div> -->
             <input type="checkbox" id="t-button" class="input-t">
             <label for="t-button" class="tooltip1" id="t-label">
-                <img src="img/perda_de_peso.png" alt="" width ="230vh" >
+                <img src="img/perda_de_peso.png" alt="" width ="230vh">
                 <span class="mensagem1">Perda de peso</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button2" class="input-t">
             <label for="t-button2" class="tooltip1" id="t-label2">
-                <span class="mensagem1">Perda de peso</span>
+            <img src="img/fortalecimento_abdominal.png" alt="" width ="135vh">
+                <span class="mensagem1">Fortalecimento abdominal</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button3" class="input-t">
             <label for="t-button3" class="tooltip1" id="t-label3">
-                <span class="mensagem1">Perda de peso</span>
+            <img src="img/manter_a_forma2.gif" alt="" width ="135vh">
+                <span class="mensagem1">Manter a forma</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button4" class="input-t">
             <label for="t-button4" class="tooltip1" id="t-label4">
-                <span class="mensagem1">Perda de peso</span>
+            <img src="img/treino_de_panturrilha.png" alt="" width ="135vh"> 
+                <span class="mensagem1">Treino de panturrilha</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button5" class="input-t">
             <label for="t-button5" class="tooltip1" id="t-label5">
-                <span class="mensagem1">Perda de peso</span>
+            <img src="img/treino_de_antebraco.png" alt="" width ="135vh">
+                <span class="mensagem1">Treino de antebraço</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button6" class="input-t">
             <label for="t-button6" class="tooltip1" id="t-label6">
-                <span class="mensagem1">Perda de peso</span>
+            <img src="img/equilibrio_do_corpo.png" alt="" width ="135vh">
+                <span class="mensagem1">Equilíbrio do corpo</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button7" class="input-t">
             <label for="t-button7" class="tooltip1" id="t-label7">
-                <span class="mensagem1">Perda de peso</span>
+            <img src="img/desenvolvimento_de_biceps.png" alt="" width ="150vh">
+                <span class="mensagem1">Desenvolver bíceps</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button8" class="input-t">
             <label for="t-button8" class="tooltip1" id="t-label8">
-                <span class="mensagem1">Perda de peso</span>
+            <img src="img/treino_de_trapezio2.png" alt="" width ="135vh">
+                <span class="mensagem1">Treino de trapézio</span>
             </label>
             </div>
         </div>
