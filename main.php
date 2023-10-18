d7328b6d51b725477efd47228447252f03053ef7
LeticiaCoelhoF <leluke20@gmail.com>
2023-10-18 18:23:44 -0300

    responsive 6.3

--- main.php
+++ main.php
@@ -27,147 +27,147 @@
             <!-- <div class="mensagem" for="t-button"></div> -->
             <input type="checkbox" id="t-button" class="input-t">
             <label for="t-button" class="tooltip1" id="t-label">
-                <img src="img/perda_de_peso.png" alt="" width ="230vh">
+                <img src="img/perda_de_peso.png" alt="" width ="230vh" class = "img1m">
                 <span class="mensagem1">Perda de peso</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button2" class="input-t">
             <label for="t-button2" class="tooltip1" id="t-label2">
-            <img src="img/fortalecimento_abdominal.png" alt="" width ="135vh">
+            <img src="img/fortalecimento_abdominal.png" alt="" width ="135vh" class = "img2m">
                 <span class="mensagem1">Fortalecimento abdominal</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button3" class="input-t">
             <label for="t-button3" class="tooltip1" id="t-label3">
-            <img src="img/manter_a_forma2.gif" alt="" width ="135vh">
+            <img src="img/manter_a_forma2.gif" alt="" width ="135vh" class = "img3m">
                 <span class="mensagem1">Manter a forma</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button4" class="input-t">
             <label for="t-button4" class="tooltip1" id="t-label4">
-            <img src="img/treino_de_panturrilha.png" alt="" width ="135vh"> 
+            <img src="img/treino_de_panturrilha.png" alt="" width ="135vh" class = "img4m"> 
                 <span class="mensagem1">Treino de panturrilha</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button5" class="input-t">
             <label for="t-button5" class="tooltip1" id="t-label5">
-            <img src="img/treino_de_antebraco.png" alt="" width ="135vh">
+            <img src="img/treino_de_antebraco.png" alt="" width ="135vh" class = "img5m">
                 <span class="mensagem1">Treino de antebraço</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button6" class="input-t">
             <label for="t-button6" class="tooltip1" id="t-label6">
-            <img src="img/equilibrio_do_corpo.png" alt="" width ="135vh">
+            <img src="img/equilibrio_do_corpo.png" alt="" width ="135vh" class = "img6m">
                 <span class="mensagem1">Equilíbrio do corpo</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button7" class="input-t">
             <label for="t-button7" class="tooltip1" id="t-label7">
-            <img src="img/desenvolvimento_de_biceps.png" alt="" width ="150vh">
+            <img src="img/desenvolvimento_de_biceps.png" alt="" width ="150vh" class = "img7m">
                 <span class="mensagem1">Desenvolver bíceps</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button8" class="input-t">
             <label for="t-button8" class="tooltip1" id="t-label8">
-            <img src="img/treino_de_trapezio2.png" alt="" width ="135vh">
+            <img src="img/treino_de_trapezio2.png" alt="" width ="135vh" class = "img8m">
                 <span class="mensagem1">Treino de trapézio</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button9" class="input-t">
             <label for="t-button9" class="tooltip1" id="t-label9">
-            <img src="img/aliviacao_dores_musculares.png" alt="" width ="140vh">
+            <img src="img/aliviacao_dores_musculares.png" alt="" width ="140vh" class = "img9m">
                 <span class="mensagem1">Aliviação das dores musculares</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button10" class="input-t">
             <label for="t-button10" class="tooltip1" id="t-label10">
-            <img src="img/fortalecimento_da_coluna001.png" alt="" width ="135vh">
+            <img src="img/fortalecimento_da_coluna001.png" alt="" width ="135vh" class = "img10m">
                 <span class="mensagem1">Fortalecimento da coluna</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button11" class="input-t">
             <label for="t-button11" class="tooltip1" id="t-label11">
-            <img src="img/treino_de_torax.gif" alt="" width ="155vh">
+            <img src="img/treino_de_torax.gif" alt="" width ="155vh" class = "img11m">
                 <span class="mensagem1">Treino de Tórax</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button12" class="input-t">
             <label for="t-button12" class="tooltip1" id="t-label12">
-            <img src="img/flexibilidade2.gif" alt="" width ="180vh">
+            <img src="img/flexibilidade2.gif" alt="" width ="180vh" class = "img12m">
                 <span class="mensagem1">Flexibilidade</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button13" class="input-t">
             <label for="t-button13" class="tooltip1" id="t-label13">
-            <img src="img/treino_de_perna.png" alt="" width ="135vh">
+            <img src="img/treino_de_perna.png" alt="" width ="135vh" class = "img13m">
                 <span class="mensagem1">Treino de pernas</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button14" class="input-t">
-            <label for="t-button14" class="tooltip1" id="t-label14">
-            <img src="img/cintura.png" alt="" width ="155vh">
+            <label for="t-button14" class="tooltip1" id="t-label14" >
+            <img src="img/cintura.png" alt="" width ="155vh"class = "img14m" >
                 <span class="mensagem1">Definição de cintura</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button15" class="input-t">
             <label for="t-button15" class="tooltip1" id="t-label15">
-            <img src="img/treino_de_triceps.png" alt="" width ="170vh">
+            <img src="img/treino_de_triceps.png" alt="" width ="170vh" class = "img15m">
                 <span class="mensagem1">Treino de tríceps</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button16" class="input-t">
             <label for="t-button16" class="tooltip1" id="t-label16">
-            <img src="img/definir_quadril.png" alt="" width ="170vh">
+            <img src="img/definir_quadril.png" alt="" width ="170vh" class = "img16m">
                 <span class="mensagem1">Definição de quadril</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button17" class="input-t">
             <label for="t-button17" class="tooltip1" id="t-label17">
-            <img src="img/ombro.png" alt="" width ="135vh">
+            <img src="img/ombro.png" alt="" width ="135vh"  class = "img17m">
                 <span class="mensagem1">Treino de ombro</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button18" class="input-t">
             <label for="t-button18" class="tooltip1" id="t-label18">
-            <img src="img/treino_dorsal.png" alt="" width ="170vh">
+            <img src="img/treino_dorsal.png" alt="" width ="170vh" class = "img18m">
                 <span class="mensagem1">Treino de dorsal</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button19" class="input-t">
             <label for="t-button19" class="tooltip1" id="t-label19">
-            <img src="img/treino_de_quadriceps.png" alt="" width ="135vh">
+            <img src="img/treino_de_quadriceps.png" alt="" width ="135vh" class = "img19m">
                 <span class="mensagem1">Treino de quadríceps</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button20" class="input-t">
             <label for="t-button20" class="tooltip1" id="t-label20">
-            <img src="img/queimar_calorias.png" alt="" width ="135vh">
+            <img src="img/queimar_calorias.png" alt="" width ="135vh" class = "img20m">
                 <span class="mensagem1">Queimar calorias</span>
             </label>
             </div>
             <div class="quadrado-shadow">
             <input type="checkbox" id="t-button21" class="input-t">
             <label for="t-button21" class="tooltip1" id="t-label21">
-            <img src="img/abdomen_lateral.png" alt="" width ="155vh">
+            <img src="img/abdomen_lateral.png" alt="" width ="155vh" class = "img21m">
                 <span class="mensagem1">Treino de abdômen lateral</span>
             </label>
             </div>
