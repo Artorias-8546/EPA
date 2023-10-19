d82edfd55bcb69d44a792a5cbe36ea143ff794fb
PedroMTG <pedroporfirio2006@gmail.com>
2023-10-19 14:54:12 -0300

    7.0update

---
+++ adicionarStar.php
@@ -0,0 +1,27 @@
+<?php
+    include 'cors_policy.php';
+	include 'conexao.php';
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // Obtém o corpo da solicitação POST
+        $data = file_get_contents("php://input");
+        // Decodifica o JSON para um objeto PHP
+        $requestData = json_decode($data);
+        // Agora você pode acessar os dados usando $requestData
+        $nome_exercicio = $requestData->nome_exercicio;
+    
+        // CodFun é o nome da coluna que está sendo enviado pelo cliente
+        $sql = "INSERT INTO favoritos VALUES (0, '$nome_exercicio');";
+    
+        if ($connection->query($sql) === true) {
+            $response = [
+                'mensagem' => 'Exercício salvo com sucesso'
+            ];
+        } else {
+            $response = [
+                'mensagem' => $connection->error
+            ];
+        }
+        echo json_encode($response);
+    }
+?>
